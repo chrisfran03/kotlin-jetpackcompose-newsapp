ce57ccb3822de5bb084ceea282c9515a13ec8265
chrisfran03 <francischristo03@gmail.com>
2025-03-20 22:00:04 +1300

    set up news list on home screen

--- app/src/main/java/com/example/newsinshort/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/newsinshort/ui/screens/HomeScreen.kt
@@ -1,20 +1,40 @@
 package com.example.newsinshort.ui.screens
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.newsinshort.ui.viewmodel.NewsViewModel
+import com.example.utilities.ResourceState
+
+const val TAG = "HomeScreen"
 
 @Composable
 fun HomeScreen(
     newsViewModel: NewsViewModel = hiltViewModel()
 ) {
+
+    val newsRes = newsViewModel.news.collectAsState()
+
     Surface(
         modifier = Modifier.fillMaxSize()
     ){
+        when(val result = newsRes.value){
+            is ResourceState.Loading -> {
+                Log.d(TAG, "Inside_Loading")
+            }
+            is ResourceState.Success -> {
+                Log.d(TAG, "Inside_Success")
 
+            }
+            is ResourceState.Error -> {
+                Log.d(TAG, "Inside_Error")
+            }
+
+        }
     }
 }
 
