a49bb0290bb1b7689edf0419933bfa98876d9edc
chrisfran03 <francischristo03@gmail.com>
2025-03-20 18:07:23 +1300

    use viewmodels in composables

--- app/src/main/java/com/example/newsinshort/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/newsinshort/ui/screens/HomeScreen.kt
@@ -4,9 +4,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.newsinshort.ui.viewmodel.NewsViewModel
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    newsViewModel: NewsViewModel = hiltViewModel()
+) {
     Surface(
         modifier = Modifier.fillMaxSize()
     ){
